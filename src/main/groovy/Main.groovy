4ae2c0aeb7fc3c8015f9f7a8ee2cec29b7a91bee
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-02-11 16:46:55 +0100

    add user monster creation

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -1,12 +1,34 @@
 static void main(String[] args)
 {
-  // Create two monsters
-  Monster dragon = new Monster("Dragon", 100, 20, 5)
-  Monster ogre = new Monster("Ogre", 120, 15, 10)
+  Scanner scanner = new Scanner(System.in)
 
-  // Start the fight
-  Fight fight = new Fight(dragon, ogre)
+  // Create Monster 1
+  println("Enter details for Monster 1:")
+  print("Name: ")
+  String name1 = scanner.nextLine()
+  print("Health: ")
+  int health1 = scanner.nextInt()
+  print("Attack Power: ")
+  int attack1 = scanner.nextInt()
+  print("Defense: ")
+  int defense1 = scanner.nextInt()
+  scanner.nextLine() // Consume any leftover newline character
+  Monster monster1 = new Monster(name1, health1, attack1, defense1)
+
+  // Create Monster 2
+  println("\nEnter details for Monster 2:")
+  print("Name: ")
+  String name2 = scanner.nextLine()
+  print("Health: ")
+  int health2 = scanner.nextInt()
+  print("Attack Power: ")
+  int attack2 = scanner.nextInt()
+  print("Defense: ")
+  int defense2 = scanner.nextInt()
+  Monster monster2 = new Monster(name2, health2, attack2, defense2)
+
+  // Start the Fight
+  Fight fight = new Fight(monster1, monster2)
   fight.start()
 
-  //i don't want anything to do with groovy! :)
 }
